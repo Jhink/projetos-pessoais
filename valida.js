192f520e02831328149d74f85daef7cf89630b33
Jhemerson <jhemerson.comercial@gmail.com>
2014-03-25 18:31:07 -0300

    Editando a validação

--- valida.js
+++ valida.js
@@ -1,3 +1,7 @@
 	$(document).ready(function{
 		alert("Hi, there!");
+		var name = prompt("Digite seu nome:");
+		var age = prompt("Digite sua idade:");
+		var text = name +", você tem "+" anos.";
+		document.getElementByID('contentBox').innerHtml=text;
 	});
